fe26fb91708f1ea427f65a604aeda7874839c6eb
tranqn <namquoctran24@gmail.com>
2026-04-28 14:30:00 +0200

    chore: add six empty apps with api/ subpackages

---
+++ backend/base_info_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BaseInfoAppConfig(AppConfig):
+    name = 'base_info_app'
---
+++ backend/offer_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OfferAppConfig(AppConfig):
+    name = 'offer_app'
