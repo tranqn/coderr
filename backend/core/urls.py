000f42d9a12100dc982fce378b57ebcc20ba8296
tranqn <namquoctran24@gmail.com>
2026-04-29 11:40:00 +0200

    feat(core): wire url includes for all six apps

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,22 +1,18 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""Root URL configuration for the Coderr project."""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include("auth_app.api.urls")),
+    path("api/", include("profile_app.api.urls")),
+    path("api/", include("offer_app.api.urls")),
+    path("api/", include("order_app.api.urls")),
+    path("api/", include("review_app.api.urls")),
+    path("api/", include("base_info_app.api.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
